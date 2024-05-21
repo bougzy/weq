13c2dd60a5e0ab3db6d95cf185702d20626b7af9
bougzy <yubeespecial@gmail.com>
2024-05-21 13:50:30 +0100

    goose

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// client/vite.config.js
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'public',  // specify the output directory
+  },
+});
