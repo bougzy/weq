027010b088ef999dee3b95ad4391923832b830f1
bougzy <yubeespecial@gmail.com>
2024-05-22 04:00:33 +0100

    yu

--- api/index.js
+++ api/index.js
@@ -119,15 +119,16 @@ app.get('/api/deposit', requireAuth, async (req, res) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
 
 
 
